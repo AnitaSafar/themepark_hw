b08685135783bcc811426a1fd821a33394a18316
Anita Safar <ancsa0416@gmail.com>
2022-01-10 17:58:09 +0000

    isAllowedTo method added to TobaccoStall, visitorAllowed test to TobaccoTest

--- ThemeParkStartPoint/src/test/java/stalls/TobaccoStallTest.java
+++ ThemeParkStartPoint/src/test/java/stalls/TobaccoStallTest.java
@@ -2,16 +2,19 @@ package stalls;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class TobaccoStallTest {
 
     TobaccoStall tobaccoStall;
+    Visitor visitor1;
 
     @Before
     public void setUp() throws Exception {
         tobaccoStall = new TobaccoStall("Jacks Drum", "Jack Jarvis", ParkingSpot.B1);
+        visitor1 = new Visitor(18, 1.8, 30);
     }
 
     @Test
@@ -27,5 +30,10 @@ public class TobaccoStallTest {
     @Test
     public void hasParkingSpot() {
         assertEquals(ParkingSpot.B1, tobaccoStall.getParkingSpot());
+    }
+
+    @Test
+    public void visitorIsAllowed() {
+        assertEquals(true, tobaccoStall.isAllowedTo(visitor1));
     }
 }
