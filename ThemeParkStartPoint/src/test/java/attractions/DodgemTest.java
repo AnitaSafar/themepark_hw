7884145d7baebed30fe5672c6f4a403e04498ead
Anita Safar <ancsa0416@gmail.com>
2022-01-10 19:36:25 +0000

    defaultPrice and priceFor methods added to dodgems, chargeHalfPriceUnder12 and chargeNormalPrice test done

--- ThemeParkStartPoint/src/test/java/attractions/DodgemTest.java
+++ ThemeParkStartPoint/src/test/java/attractions/DodgemTest.java
@@ -2,12 +2,14 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor visitor;
 
     @Before
     public void setUp() throws Exception {
@@ -28,5 +30,17 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void chargeHalfPriceUnder12() {
+        visitor = new Visitor(10, 1.2, 5);
+        assertEquals(2.25, dodgems.priceFor(visitor), 0.1);
+    }
+
+    @Test
+    public void chargeNormalPrice() {
+        visitor = new Visitor(14, 1.5, 20);
+        assertEquals(4.50, dodgems.priceFor(visitor), 0.1);
     }
 }
